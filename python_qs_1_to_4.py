a68a44aa484535baafb2c77c9e3695cb959e51d8
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-10-30 16:05:36 +0530

    Adding a function for checking palindrome words.
    The function will return 'True' or 'False' by checking the string.
    
    I have completed second question. Iam deleting my first question from the file and rewriting the file with second question.

--- python_qs_1_to_4.py
+++ python_qs_1_to_4.py
@@ -1,28 +1,15 @@
-    
-
-#QUESTION 1. Write a python function called fizzbizz which will take a
-    #    number n as input and print numbers i from 1 to n. 
-    #     a. If i is divisible by 3, it will print fizz instead
-    #        of i
-    #     b. If i is divisible by 5, it will print bizz instead
-    #        of i
-    #     c. If i is divisible by 15, it will print fizzbizz
-    #        instead i
-    #     d. Otherwise, it will just print i
-
-def fizzbizz(n):
-    n=int(input("enter the number :  "))
-    for i in range(1,n+1):
-        if i % 3 == 0 and i % 5 != 0 and i % 15 != 0:
-            print("fizz")
-        elif i % 5 ==0 and i % 3 !=0 and i % 15 != 0:
-            print("bizz")
-        elif i % 15 ==0 and i % 3 ==0 and i % 5 == 0:
-            print("fizzbizz")
-
-        else:
-            print(i)
+    # 2. Write a python function called palindrome that receives a string s as an
+    #    argument. It will return True if s is a palindrome and False if
+    #    not. A palindrome is a word that will read the same from left
+    #    to right and right to left. e.g. "dad", "abba"
 
 
+def palindrome(s):
+    s=input("enter a word  :  ")
+    if s == s[::-1]:
+        print("True")
+    else:
+        print("False")
 
-fizzbizz(" ") 
+
+palindrome('')
