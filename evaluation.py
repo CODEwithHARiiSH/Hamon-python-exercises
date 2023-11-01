5282fe0308e3c6a7a47ab0e14949586e4086c64c
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-01 15:46:08 +0530

    Green:Implements function for multiplication test fails

--- evaluation.py
+++ evaluation.py
@@ -1,13 +1,15 @@
 def evaluat(x):
     stack=[]
     for i in x:
-        if i in ["+" , "-"]:
+        if i in ["+" , "-" , "*"]:
             operand_1=stack.pop()
             operand_2=stack.pop()
             if i == "+":
                 stack.append(operand_1+operand_2)
             elif i == "-":
                 stack.append(operand_2-operand_1)
+            elif i == "*":
+                stack.append(operand_1*operand_2)
         else:
             stack.append(int(i))
     return stack.pop()
