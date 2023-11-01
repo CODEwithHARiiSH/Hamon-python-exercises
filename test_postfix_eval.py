bbd89bb73ef4e0635cec1f977594e1c8cf97f262
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-01 15:41:56 +0530

    Green:Implements fuction for minus test success

--- test_postfix_eval.py
+++ test_postfix_eval.py
@@ -1,10 +1,10 @@
 from evaluation import *
 
 def test_single_operator():
-    assert evaluat("2") == 2
+    assert evaluat("9") == 9
     
 def test_operand_addition():
-    assert evaluat("24+") == 6
+    assert evaluat("94+") == 13
     
-def test_operand_addition():
+def test_operand_minus():
     assert evaluat("94-") == 5
