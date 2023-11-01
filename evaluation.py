0a1f7adecb06f6208ee6f63aed5d04d4e243f3e7
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-01 15:17:51 +0530

    Green:Implements function for addition test sussess

--- evaluation.py
+++ evaluation.py
@@ -1,2 +1,12 @@
 def evaluat(x):
-    return int(x)
+    stack=[]
+    for i in x:
+        if i in ["+"]:
+            operand_1=stack.pop()
+            operand_2=stack.pop()
+            stack.append(operand_1+operand_2)
+        else:
+            stack.append(int(i))
+    return stack.pop()
+    
+    
--- test_postfix_eval.py
+++ test_postfix_eval.py
@@ -4,4 +4,4 @@ def test_single_operator():
     assert evaluat("2") == 2
     
 def test_operand_addition():
-    assert evaluat("2+4") == 6
+    assert evaluat("24+") == 6
