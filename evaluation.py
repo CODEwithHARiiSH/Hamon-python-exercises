b7e9642b6adff8b4831dcaf7244ddf07501ed556
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 10:28:03 +0530

    Green:implements function for only alphabets, test success

--- evaluation.py
+++ evaluation.py
@@ -1,30 +1,34 @@
 def evaluat(x):
     stack=[]
-    for i in x:
-        char='abcdefghijklmnopqrstuvwxizABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        if i in char:
-            continue
-        else:
-            if i in ["+" , "-" , "*" , "/"]:
-                operand_1=stack.pop()
-                operand_2=stack.pop()
-                if i == "+":
-                    stack.append(operand_1+operand_2)
-                elif i == "-":
-                    stack.append(operand_2-operand_1)
-                elif i == "*":
-                    stack.append(operand_1*operand_2)
-                elif i == "/":
-                    stack.append(operand_2//operand_1)
+    try:
+            for i in x:
+                char='abcdefghijklmnopqrstuvwxizABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                if i in char:
+                    continue
+                else:
+                    if i in ["+" , "-" , "*" , "/"]:
+                        operand_1=stack.pop()
+                        operand_2=stack.pop()
+                    if i == "+":
+                        stack.append(operand_1+operand_2)
+                    elif i == "-":
+                        stack.append(operand_2-operand_1)
+                    elif i == "*":
+                        stack.append(operand_1*operand_2)
+                    elif i == "/":
+                        stack.append(operand_2//operand_1)
 
-            else:
-                stack.append(int(i))
-    return stack.pop()
+                    else:
+                        stack.append(int(i))
+            return stack.pop()
+    except:
+        return "Invalid Inputs"
+
+
     
     
     
     
 if __name__ == '__main__':
-    print(evaluat("aa44+"))
-    
+    print(evaluat("aa"))
     
