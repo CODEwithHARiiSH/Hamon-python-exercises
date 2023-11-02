f612e024429502d00ff60c4f822b736fab75fd63
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 10:19:12 +0530

    Grees:Implements function for alphabet , test success.

--- evaluation.py
+++ evaluation.py
@@ -1,26 +1,30 @@
 def evaluat(x):
     stack=[]
     for i in x:
-        if i in ["+" , "-" , "*" , "/"]:
-            operand_1=stack.pop()
-            operand_2=stack.pop()
-            if i == "+":
-                stack.append(operand_1+operand_2)
-            elif i == "-":
-                stack.append(operand_2-operand_1)
-            elif i == "*":
-                stack.append(operand_1*operand_2)
-            elif i == "/":
-                stack.append(operand_2//operand_1)
-
+        char='abcdefghijklmnopqrstuvwxizABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        if i in char:
+            continue
         else:
-            stack.append(int(i))
+            if i in ["+" , "-" , "*" , "/"]:
+                operand_1=stack.pop()
+                operand_2=stack.pop()
+                if i == "+":
+                    stack.append(operand_1+operand_2)
+                elif i == "-":
+                    stack.append(operand_2-operand_1)
+                elif i == "*":
+                    stack.append(operand_1*operand_2)
+                elif i == "/":
+                    stack.append(operand_2//operand_1)
+
+            else:
+                stack.append(int(i))
     return stack.pop()
     
     
     
     
 if __name__ == '__main__':
-    print(evaluate("44+"))
+    print(evaluat("aa44+"))
     
     
